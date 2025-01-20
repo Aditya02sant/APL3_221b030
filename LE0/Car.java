21223113d625933b714d02453468057005048fdc
221B030 <221B030@juet.co.in>
2025-01-20 15:33:19 +0530

    e4

---
+++ LE0/Car.java
@@ -0,0 +1,17 @@
+public class Car{
+     private String name;
+     private int speed;
+    
+     public String getname(){
+        return name;
+   }
+     public void setname(String newname){
+        this.name=newname;
+     }
+    public int getspeed(){
+        return speed;
+     }
+     public void setspeed(int newspeed){
+         this.speed=newspeed;
+   }
+ }
---
+++ LE0/Main.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    String name;
+	    int speed;
+	    int x=Integer.parseInt(args[0]);
+		System.out.println("Happy New Year "+x);
+		
+		Car myobj= new Car();
+		myobj.setname("Aditya Sant");
+		System.out.println(myobj.getname());
+		Car myobj1= new Car();
+		myobj1.setspeed(75);
+		System.out.println(myobj1.getspeed());
+	}
+
+
+}
