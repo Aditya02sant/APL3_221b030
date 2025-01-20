9060c7647c8ce1e4da1b0ca9884f57e2bac2740a
221B030 <221B030@juet.co.in>
2025-01-20 15:43:57 +0530

    LAb exercise 1

---
+++ LE1/PROB_3/Child.java
@@ -0,0 +1,13 @@
+
+//for the 1st case 
+
+public class Child extends Mother{
+    static void show(){
+        System.out.println("This is Child show");
+    }
+}
+
+// result 
+//this is mother show 
+//this is Child show 
+//this is mother show 
---
+++ LE1/PROB_3/Mother.java
@@ -0,0 +1,17 @@
+
+
+//for the 1st case 
+
+public class Mother{
+    int x;
+    
+    static void show(){
+        System.out.println("This is Mother show");
+    }
+}
+
+// result 
+//this is mother show 
+//this is Child show 
+//this is mother show 
+
